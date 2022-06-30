50b60f4185374c0f86c27f4f16ae54c370f80b97
sharran04 <ransha04@outlook.com>
2022-06-30 12:51:35 +0000

    provider added

--- mynewfile/provider.tf
+++ mynewfile/provider.tf
@@ -10,4 +10,10 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id   = "02953bee-ef8c-472d-8407-5dc39d59c79c"
+  tenant_id         = "438c81e2-2a47-4d31-b7dc-12b1069bcfda"
+  client_id         = "dea217e3-2f4b-4d62-9d79-7626e46f89af"
+  client_secret     = "4L.8Q~_WgWSA1_ciYqsaoG6CS.4HBbxoPVagaayd"
+
 }
