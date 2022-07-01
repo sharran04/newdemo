c0c571284f6a70913e5f70d9018eb70581731dc0
sharran04 <ransha04@outlook.com>
2022-07-01 06:34:11 +0000

    added vnet after auto

--- mynewfile/modules/RG/main.tf
+++ mynewfile/modules/RG/main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "rg" {
   name      = random_pet.rg-name.id
   location  = var.resource_group_location
 }
+
+resource "azurerm_virtual_network" "example" {
+name = "myvnet"
+resource_group_name = azurerm_resource_group.rg.name
+location = azurerm_resource_group.rg.location
+address_space = ["10.0.0.0/16"]
+}
